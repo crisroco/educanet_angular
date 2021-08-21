19c7d28da18427fc47a7e60afb5ee8e7c2344bc3
Edmundo <eacostac@cientifica.edu.pe>
2021-08-22 07:37:53 +1100

    strm

--- src/app/services/docente.service.ts
+++ src/app/services/docente.service.ts
@@ -208,7 +208,7 @@ export class DocenteService {
 
     public getLinkZoom(cicle, myclass, date, section, teacher): Promise<any> {
         let url = "https://cientificavirtual.cientifica.edu.pe//mod/zoom/client/zoom_link.php?strm=";
-        if(cicle == '1072' || cicle == '1073' || cicle == '1117' || cicle == '1118' || cicle == '1156' || cicle == '1157' || cicle == '2220' || cicle == '2222' || cicle == '2225' || cicle == '2235' || cicle == '2237' || cicle == '2238'){
+        if(cicle == '1072' || cicle == '1073' || cicle == '1117' || cicle == '1118' || cicle == '1156' || cicle == '1157' || cicle == '2220' || cicle == '2222' || cicle == '2225' || cicle == '2228' || cicle == '2235' || cicle == '2237' || cicle == '2238'){
             url = "https://aulavirtualcpe.cientifica.edu.pe/mod/zoom/client/zoom_link.php?strm=";
         }
         return this.http.get(url + cicle + '&nbr=' + myclass + '&date=' + date + '&section=' + section + '&teacher=' + teacher, {responseType: 'text'}).toPromise();
