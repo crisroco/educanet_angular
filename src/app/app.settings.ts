5ed746a438c2196b0b6eb03aba2a1e201ca9a984
Edmundo <eacostac@cientifica.edu.pe>
2021-12-17 00:38:00 -0500

    SSH

--- src/app/app.settings.ts
+++ src/app/app.settings.ts
@@ -55,24 +55,24 @@ export class AppSettings {
         // { value: "00", name: 'NO CUENTA CON INFORMACIÓN' },
     ]
     public static ACCESS_VAC = { "action": "login", "name": "admin", "pass": "aic37896" };
-    public static URL_LOGIN  = "http://login-ad.educad.pe/";
-    public static BASE_NODEJS = 'http://apirest.educad.pe:3000';
-    public static SERVICES_EDUCAD = "http://wsasistencia.educad.pe";
+    public static URL_LOGIN  = "https://login-ad.educad.pe/";
+    public static BASE_NODEJS = 'https://apirest.educad.pe:3000';
+    public static SERVICES_EDUCAD = "https://wsasistencia.educad.pe";
     public static BASE_DESEMPENO_DOCENTE = "https://back-desempeno.educad.pe/";
     public static SERVICES_INCORPORACION = "https://back-incorporacion.educad.pe/api";
-    public static BASE_SISE_CODEIGNITER = 'http://proyectos.educad.pe';
+    public static BASE_SISE_CODEIGNITER = 'https://proyectos.educad.pe';
     //------------------------------- ENV PRODUCTION -------------------------------//
     // public static BASE_UCSUR_LARAVEL = 'http://dev3.pasarela.educad.pe/api'; // FAKE PROD
-    public static BASE_UCSUR_LARAVEL = 'http://dev.pasarela.educad.pe/api'; // REAL PROD
+    public static BASE_UCSUR_LARAVEL = 'https://pasarela.educad.pe/api'; // REAL PROD
     public static BASE_SISE_LARAVEL = 'https://pasarela.sise.edu.pe/api';
     public static BASE_UCSUR_LARAVEL_AUTH = AppSettings.BASE_UCSUR_LARAVEL + '/auth';
-    public static PRODUCTION = "http://educanet.back.educad.pe";
+    public static PRODUCTION = "https://educanet-back.educad.pe";
     public static DISPO = "https://cuadernosevaluacion-back.cientifica.edu.pe/v1/";
     //------------------------------- ENV DEVELOP -------------------------------//
     // public static BASE_UCSUR_LARAVEL = 'http://dev2.pasarela.educad.pe/api';
     // public static BASE_UCSUR_LARAVEL_AUTH = 'http://dev2.pasarela.educad.pe/api/auth';
     // public static BASE_SISE_LARAVEL  = 'https://dev-pasarela.sise.edu.pe/api';
-    // public static PRODUCTION = "http://dev-educanet.back.educad.pe";
+    // public static PRODUCTION = "https://educanet-back.educad.pe";
     // public static DISPO = "https://dev-disponibilidad-back.educad.pe/v1/";
     //------------------------------- ENV DEVELOP -------------------------------//
     public static BASE_PATH_DENUNCIA = AppSettings.PRODUCTION + '/denuncia/';
@@ -81,7 +81,7 @@ export class AppSettings {
     public static WS_DRUPAL_GENERARTOKEN = AppSettings.PRODUCTION+'/session/token';
     public static WS_DRUPAL_LOGINVACACIONES = AppSettings.PRODUCTION+'/vacaciones/user';
     public static BASE_PATH = AppSettings.PRODUCTION + '/resources_portal/';
-    public static BASE_PAYMENT_HISTORY = 'http://200.121.226.233/ServicioRestWebFRACTAL/api/DescargarBoletaRemuneracion';
-    public static BASE_FRACTAL = 'http://fractal.grupoeducad.edu.pe/ServicioRestWebFRACTAL/api';
-    public static MOODLE = "http://aulavirtual.educad.pe/local/wseducad/auth/sso.php?";
+    public static BASE_PAYMENT_HISTORY = 'https://fractal.grupoeducad.edu.pe/ServicioRestWebFRACTAL/api/DescargarBoletaRemuneracion';
+    public static BASE_FRACTAL = 'https://fractal.grupoeducad.edu.pe/ServicioRestWebFRACTAL/api';
+    public static MOODLE = "https://aulavirtual.educad.pe/local/wseducad/auth/sso.php?";
 }
